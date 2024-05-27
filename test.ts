b9394ebd8e24e448c43ae5a358fc1a3069360295
lituneee <bswjtsnpt@gmail.com>
2024-05-27 21:25:12 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -30,3 +30,45 @@ export default App;
 
 
 
+
+
+
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const useKeepAlive = (intervalMinutes) => {
+    useEffect(() => {
+        const interval = setInterval(() => {
+            axios.get('https://your-api-endpoint.com/health', { withCredentials: true })
+                .then(response => {
+                    console.log('Keep-alive successful:', response.data);
+                })
+                .catch(error => {
+                    console.error('Keep-alive failed:', error);
+                });
+        }, intervalMinutes * 60 * 1000); // Convert minutes to milliseconds
+
+        return () => clearInterval(interval); // Cleanup interval on component unmount
+    }, [intervalMinutes]);
+};
+
+export default useKeepAlive;
+
+
+
+
+
+import React from 'react';
+import useKeepAlive from './useKeepAlive';
+
+const App = () => {
+    useKeepAlive(15);
+    return (
+        <div>
+            {/* Your application code */}
+        </div>
+    );
+};
+
+export default App;
+
