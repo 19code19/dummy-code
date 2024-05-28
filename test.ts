509ba46c249f56e33ed8f4a866c564818f692668
lituneee <bswjtsnpt@gmail.com>
2024-05-28 17:38:18 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,74 +1,11 @@
-import { useEffect } from 'react';
-
-const useKeepAlive = () => {
-    useEffect(() => {
-        const interval = setInterval(() => {
-            axios.get('https://your-api-endpoint.com/keep-alive', { withCredentials: true });
-        }, 15 * 60 * 1000); // Every 15 minutes
-
-        return () => clearInterval(interval);
-    }, []);
-};
-
-export default useKeepAlive;
-
-
-
-import React from 'react';
-import useKeepAlive from './useKeepAlive';
-
-const App = () => {
-    useKeepAlive();
-    return (
-        <div>
-            {/* Your application code */}
-        </div>
-    );
-};
-
-export default App;
-
-
-
-
-
-
-import { useEffect } from 'react';
-import axios from 'axios';
-
-const useKeepAlive = (intervalMinutes) => {
-    useEffect(() => {
-        const interval = setInterval(() => {
-            axios.get('https://your-api-endpoint.com/health', { withCredentials: true })
-                .then(response => {
-                    console.log('Keep-alive successful:', response.data);
-                })
-                .catch(error => {
-                    console.error('Keep-alive failed:', error);
-                });
-        }, intervalMinutes * 60 * 1000); // Convert minutes to milliseconds
-
-        return () => clearInterval(interval); // Cleanup interval on component unmount
-    }, [intervalMinutes]);
-};
-
-export default useKeepAlive;
-
-
-
-
-
-import React from 'react';
-import useKeepAlive from './useKeepAlive';
-
-const App = () => {
-    useKeepAlive(15);
-    return (
-        <div>
-            {/* Your application code */}
-        </div>
-    );
-};
-
-export default App;
-
+SELECT 
+    dcode(date_column, 
+          '00000' null, 
+          CASE
+              WHEN date_column IS NOT NULL AND LENGTH(date_column) = 8 THEN 
+                  TO_CHAR(TO_DATE(date_column, 'YYYYMMDD'), 'DD-Mon-YYYY')
+              ELSE 
+                  'Invalid Date'
+          END
+    )
+FROM your_table;
