3a73f4488f606907824242526de9be901f660654
lituneee <bswjtsnpt@gmail.com>
2024-05-29 11:37:47 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,11 +1,20 @@
-SELECT 
-    dcode(date_column, 
-          '00000' null, 
-          CASE
-              WHEN date_column IS NOT NULL AND LENGTH(date_column) = 8 THEN 
-                  TO_CHAR(TO_DATE(date_column, 'YYYYMMDD'), 'DD-Mon-YYYY')
-              ELSE 
-                  'Invalid Date'
-          END
-    )
-FROM your_table;
+SELECT
+    DECODE(
+        CASE 
+            WHEN LENGTH(AU.CAPITAL_AS_OF_DATE) = 8 
+                 AND REGEXP_LIKE(AU.CAPITAL_AS_OF_DATE, '^[0-9]{8}$')
+            THEN
+                CASE
+                    WHEN TO_DATE(SUBSTR(AU.CAPITAL_AS_OF_DATE, 1, 4) || '-' || 
+                                 SUBSTR(AU.CAPITAL_AS_OF_DATE, 5, 2) || '-' || 
+                                 SUBSTR(AU.CAPITAL_AS_OF_DATE, 7, 2), 'YYYY-MM-DD') IS NOT NULL
+                    THEN 1
+                    ELSE 0
+                END
+            ELSE 0
+        END,
+        1, 
+        To_Char(TO_DATE(AU.CAPITAL_AS_OF_DATE, 'YYYYMMDD'), 'DD-Mon-YYYY'), 
+        NULL
+    ) AS formatted_date
+FROM YourTable;
