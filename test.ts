2cbb9e6200b9bd60bd14205827b50af0a3f5a2b7
lituneee <bswjtsnpt@gmail.com>
2024-05-29 12:00:03 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,20 +1,10 @@
-SELECT
-    DECODE(
-        CASE 
-            WHEN LENGTH(AU.CAPITAL_AS_OF_DATE) = 8 
-                 AND REGEXP_LIKE(AU.CAPITAL_AS_OF_DATE, '^[0-9]{8}$')
-            THEN
-                CASE
-                    WHEN TO_DATE(SUBSTR(AU.CAPITAL_AS_OF_DATE, 1, 4) || '-' || 
-                                 SUBSTR(AU.CAPITAL_AS_OF_DATE, 5, 2) || '-' || 
-                                 SUBSTR(AU.CAPITAL_AS_OF_DATE, 7, 2), 'YYYY-MM-DD') IS NOT NULL
-                    THEN 1
-                    ELSE 0
-                END
-            ELSE 0
-        END,
-        1, 
-        To_Char(TO_DATE(AU.CAPITAL_AS_OF_DATE, 'YYYYMMDD'), 'DD-Mon-YYYY'), 
-        NULL
-    ) AS formatted_date
+SELECT 
+    CASE 
+        WHEN AU.CAPITAL_AS_OF_DATE = '00000000' THEN NULL
+        WHEN LENGTH(AU.CAPITAL_AS_OF_DATE) = 8 
+             AND REGEXP_LIKE(AU.CAPITAL_AS_OF_DATE, '^[0-9]{8}$') 
+             AND TO_DATE(AU.CAPITAL_AS_OF_DATE, 'YYYYMMDD') IS NOT NULL 
+        THEN TO_CHAR(TO_DATE(AU.CAPITAL_AS_OF_DATE, 'YYYYMMDD'), 'DD-Mon-YYYY')
+        ELSE NULL
+    END AS formatted_date
 FROM YourTable;
